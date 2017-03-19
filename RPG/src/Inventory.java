8fb87cc90df9356aaea222831e3795605d20ba33
Luke Creighton <lk479778@dal.ca>
2017-03-19 16:07:12 -0300

    //Some more stuff

--- RPG/src/Inventory.java
+++ RPG/src/Inventory.java
@@ -1,4 +1,13 @@
-
+import java.util.*;
 public class Inventory {
-
+	private LinkedList items;
+	
+	public Inventory(){
+		items = new LinkedList();
+	}
+	
+	public void addItem(){
+		
+	}
+	
 }
--- RPG/src/Player.java
+++ RPG/src/Player.java
@@ -1,5 +1,11 @@
 public class Player {
 	private int hp;
 	private String name;
+	private Inventory pack;
+	
+	public Player(String n){
+		name = n;
+		pack = new Inventory();
+	}
 	
 }
