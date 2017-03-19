9b5ab1d628fd8e8e52d55c544a4e592188de4fe0
Luke Creighton <lk479778@dal.ca>
2017-03-19 16:16:35 -0300

    //MORE OBJECTS

--- RPG/src/Inventory.java
+++ RPG/src/Inventory.java
@@ -1,13 +1,26 @@
 import java.util.*;
 public class Inventory {
 	private LinkedList items;
+	private int count;
 	
 	public Inventory(){
 		items = new LinkedList();
+		count = 0;
+	}
+	//SETTERS AND GETTERS
+	//TODO: SETTERS AND GETTERS
+	//END OF SETTERS AND GETTERS
+	
+	public void addItem(Weapon w){
+		items.add(w);
+		count++;
 	}
 	
 	public void addItem(){
 		
 	}
+	
+	
+	
 	
 }
